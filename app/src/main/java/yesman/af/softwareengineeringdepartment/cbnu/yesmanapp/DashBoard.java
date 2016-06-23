9ee03d60fee1d6e61d223c6795fe94bcfd90930a
ko <go@marusoft.net>
2016-06-23 17:10:27 +0900

    DashBoard User 변수 수정

--- app/src/main/java/yesman/af/softwareengineeringdepartment/cbnu/yesmanapp/DashBoard.java
+++ app/src/main/java/yesman/af/softwareengineeringdepartment/cbnu/yesmanapp/DashBoard.java
@@ -10,9 +10,72 @@ public class DashBoard {
     private double y;
     private String title;
     private String content;
+    private int domain;
+    private String requestID;
+    private String acceptID;
+    private int category;
+    private int ischeckrequest;
+    private int ischeckaccept;
+    private int ismatching;
     private Date date;
 
-    public DashBoard(String title,String content,Date date,double x,double y){
+    public int getDomain() {
+        return domain;
+    }
+
+    public void setDomain(int domain) {
+        this.domain = domain;
+    }
+
+    public String getRequestID() {
+        return requestID;
+    }
+
+    public void setRequestID(String requestID) {
+        this.requestID = requestID;
+    }
+
+    public String getAcceptID() {
+        return acceptID;
+    }
+
+    public void setAcceptID(String acceptID) {
+        this.acceptID = acceptID;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getIscheckrequest() {
+        return ischeckrequest;
+    }
+
+    public void setIscheckrequest(int ischeckrequest) {
+        this.ischeckrequest = ischeckrequest;
+    }
+
+    public int getIscheckaccept() {
+        return ischeckaccept;
+    }
+
+    public void setIscheckaccept(int ischeckaccept) {
+        this.ischeckaccept = ischeckaccept;
+    }
+
+    public int getIsmatching() {
+        return ismatching;
+    }
+
+    public void setIsmatching(int ismatching) {
+        this.ismatching = ismatching;
+    }
+
+    public DashBoard(String title, String content, Date date, double x, double y){
         this.x = x;
         this.y = y;
         this.title = title;
